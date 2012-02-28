de799d4b7d230aefbeed4cc3e97c1fa11ddce328
Wilker Lucio <wilkerlucio@gmail.com>
2012-02-28 20:09:36 -0300

    config vendor for base64 lib

--- vendor/vendorjs.js
+++ vendor/vendorjs.js
@@ -6,9 +6,10 @@
     order: "order",
 
     // libraries
-    jquery:    "jquery-1.7.1.min",
-    ember:     "ember-loader",
-    ember_lib: "ember-0.9.5",
+    jquery:        "jquery-1.7.1.min",
+    ember:         "ember-loader",
+    base64_binary: "base64-binary",
+    ember_lib:     "ember-0.9.5",
   };
 
   window.vendorjsPaths = function vendorjsPaths(paths, prefix) {
